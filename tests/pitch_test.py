3d0ed29733accc9076aaa5b1668563ef7a96b9b2
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-06 02:50:06 +0300

    add test for pitch class

---
+++ tests/pitch_test.py
@@ -0,0 +1,21 @@
+import unittest
+from app.models import Pitch
+# Pitch = pitch.Pitch
+
+class PitchTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Pitch class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_pitch = Pitch(1,'interview','blaaaaaaaah blaaaaaaaaaaaaaaaaaaah')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitch))
+
+
+if __name__ == '__main__':
+    unittest.main()
