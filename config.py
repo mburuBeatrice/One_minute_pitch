1c4775f7f0a1382bc50b51e18a1f306c5fa671f3
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-06 03:09:04 +0300

    add environment and db URI

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://beatrice shiro:ciru6254#@localhost/pitch'
-
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://beatrice_shiro:ciru6254#@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
     '''
@@ -25,3 +26,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
