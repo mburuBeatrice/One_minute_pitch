765f95c00b57cd7e5f70bf74fa7f20e99ec2902b
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-06 03:04:12 +0300

    add a WTF form

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect, url_for
+from . import main
+from .forms import CommentForm
+from ..models import Comment
 
+# Comment = comment.Comment
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -11,4 +14,29 @@ def index():
     title = 'One Minute Pitch!!!'
     
     return render_template('index.html', title = title)
-    
+@main.route('/pitch/<int:id>')
+def pitch(id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    title = 'welcome to this awesome app'
+    comments = Comment.get_comments(pitch.id)
+
+    return render_template('movie.html',title = title,pitch = pitch,comments = comments) 
+
+@main.route('/pitch/comment/', methods = ['GET','POST'])
+def new_comment(id):
+    form = CommentForm()
+
+
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.comment.data
+
+        new_comment = Comment(id=comment.id,title=title,comment=comment,user=current_user)
+
+        new_comment.save_comment()
+        return redirect(url_for('.pitch',id = pitch.id ))
+
+    return render_template('new_comment.html',comment_form=form)    
