0b7835796dbf44cba561a0dfbec1ec2b3d865103
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-06 03:07:43 +0300

    add blueprint register and also db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
 
-#Initializing application
-app = Flask(__name__)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
+def create_app(config_name):
 
-from app import views
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Will add the views and forms
+
+    return app
