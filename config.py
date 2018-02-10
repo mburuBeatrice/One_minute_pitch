df3ac3de13b90bc3689851f7150937b398a3f91e
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-10 11:43:25 +0300

    add secret key

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://beatrice_shiro:ciru6254@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
